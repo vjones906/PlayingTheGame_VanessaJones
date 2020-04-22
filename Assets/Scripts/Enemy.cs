c914cce1328df99beda5d5218304374093673a47
Vanessa <vanessamaikojones@gmail.com>
2020-04-22 10:43:44 -0800

    lesson 4B

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody enemyRB;
+    private GameObject player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce(moveDir * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private GameObject player;
+    private const float randomRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Instantiate(enemyPrefab, player.transform.position + GenSpawnPos(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GenSpawnPos()
+    {
+        float spawnX = Random.Range(-randomRange, randomRange);
+        float spawnZ = Random.Range(-randomRange, randomRange);
+        Vector3 pos = new Vector3(spawnX, 0, spawnZ);
+
+        return pos;
+    }
+}
