6bb2bebcf059948feb98bc87fc5dd12012e67373
Vanessa <vanessamaikojones@gmail.com>
2020-04-29 11:33:20 -0800

    lesson 4C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     public float forSpeed;
     private GameObject focalPoint;
     private Renderer playerRend;
+    public bool hasPowerUp = false;
+    private float powerUpKick = 10.0f;
+    private Animator anim;
+    public GameObject powerUpIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,7 @@ public class PlayerController : MonoBehaviour
         playerRB = GetComponent<Rigidbody>();
         playerRend = GetComponent<Renderer>();
         focalPoint = GameObject.Find("Focal Point");
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +35,38 @@ public class PlayerController : MonoBehaviour
         else
         {
             playerRend.material.color = new Color(1 + verticalInput, 1, 1 + verticalInput);
+        }
+        powerUpIndicator.transform.position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            anim.SetBool("Has Power Up", true);
+            powerUpIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdown());
+        }
+    }
+
+    private IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(5.0f);
+        anim.SetBool("Has Power Up", false);
+        powerUpIndicator.SetActive(false);
+        hasPowerUp = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Player collided with " + collision.gameObject + " with powerUp set to " + hasPowerUp);
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 dir = collision.gameObject.transform.position - transform.position;
+            enemyRB.AddForce(dir * powerUpKick, ForceMode.Impulse);
         }
     }
 }
